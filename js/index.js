e5994ff429c68a177b8d8708d787cf7bd181d818
Ambia-Ela <ambiaela7275@gmail.com>
2024-09-21 19:20:55 +0600

    totall expense done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
 const calculateButton = document.getElementById('calculate').addEventListener('click', function (event) {
-    event.preventDefault();
+    // event.preventDefault(); form ar jonno use only
     const income = parseFloat(document.getElementById('income').value);
     const software = parseFloat(document.getElementById('software').value);
     const courses = parseFloat(document.getElementById('courses').value);
     const internet = parseFloat(document.getElementById('internet').value);
 
     console.table({ income, software, courses, internet });
+
+
+    const totalExpenses = software + courses + internet;
+
+    const balance = income - totalExpenses;
+
+    console.table({ totalExpenses, balance });
    
+    const totalExpensesElement = document.getElementById('total-expenses');
+    totalExpensesElement.innerText = totalExpenses.toFixed(2);
+
+    const balanceElement = document.getElementById('balance');
+    balanceElement.innerText = balance.toFixed(2);
+
+    // show result class and omit hidden cls in js 
+    const result = document.getElementById('results');
+    result.classList.remove('hidden');
     
     
 })
