8ae34056c82ea094a16d5088718f31b710d9dc66
Ambia-Ela <ambiaela7275@gmail.com>
2024-09-21 21:28:21 +0600

    totall history  done

--- js/index.js
+++ js/index.js
@@ -9,7 +9,6 @@ const calculateButton = document.getElementById('calculate').addEventListener('c
 
 
     const totalExpenses = software + courses + internet;
-
     const balance = income - totalExpenses;
 
     console.table({ totalExpenses, balance });
@@ -24,5 +23,68 @@ const calculateButton = document.getElementById('calculate').addEventListener('c
     const result = document.getElementById('results');
     result.classList.remove('hidden');
     
+    const historyItem = document.createElement('div');
+    historyItem.className = "bg-white p-3 rounded-md border-l-2 border-indigo-500";
+
+    historyItem.innerHTML = `
+    <p class="text-xs text-gray-800 font-bold">${new Date().toLocaleDateString()}</p>
+    <p class="text-xs text-gray-800 font-bold">Income:${income.toFixed(2)}</p>
+    <p class="text-xs text-gray-800 font-bold">Expenxes:${totalExpenses.toFixed(2)}</p>
+    <p class="text-xs text-gray-800 font-bold">Balance:${balance.toFixed(2)}</p>
+
+    `
     
+    const historyContainer = document.getElementById('history-list');
+    // insertBefor 2 ta value create kore (ki insert korbo and kothai insert korbo)
+    historyContainer.insertBefore(historyItem,historyContainer.firstChild)
+});
+
+// calculate-savings button
+
+document.getElementById('calculate-savings').addEventListener('click', function () {
+
+    const savingPercentace = parseFloat(document.getElementById('savings').value);
+
+    const income = parseFloat(document.getElementById('income').value);
+    const software = parseFloat(document.getElementById('software').value);
+    const courses = parseFloat(document.getElementById('courses').value);
+    const internet = parseFloat(document.getElementById('internet').value);
+    
+    const totalExpenses = software + courses + internet;
+    const balance = income - totalExpenses;
+
+    const savingAmount = (savingPercentace * balance) / 100;
+
+    // remainig balance show
+    const remainingBalance = balance - savingAmount;
+    
+    // saving amount a dekhate chassi
+    const savingElement = document.getElementById('savings-amount');
+    savingElement.innerText = savingAmount.toFixed(2);
+
+    // remaining balance dekte chassi
+    const remainingElement = document.getElementById('remaining-balance');
+    remainingElement.innerText = remainingBalance.toFixed(2);
+   
+});
+
+// history tab funcunality 
+     const historyTab = document.getElementById('history-tab');
+    const assistentTab = document.getElementById('assistant-tab');
+     
+     historyTab.addEventListener('click', function () {
+    historyTab.classList.add
+             ('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+         historyTab.classList.remove('text-gray-600');
+         assistentTab.classList.remove(
+             'text-white', 'bg-gradient-to-r', 'from-blue-500',
+             'to-purple-600'
+         );
+         assistentTab.classList.add('text-gray-600');
+
+         //  remove expense form hidden
+         document.getElementById('expense-form').classList.add('hidden');
+
+         const historyRemove = document.getElementById('history-section');
+         historyRemove.classList.remove('hidden');
 })
